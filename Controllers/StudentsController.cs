8db87725ba701cf3b6f48efce07f7167f1caab07
PC <201910310@ua.edu.lb>
2022-06-28 05:55:58 -0700

    2nd commit

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -22,7 +22,8 @@ public class StudentsController : ControllerBase
     {
         new Student(1,"Stud1","Stud1@gmail.com"),
         new Student(2,"Stud2","Stud2@gmail.com"),
-        new Student(3,"Stud3","Stud3@gmail.com")
+        new Student(3,"Stud3","Stud3@gmail.com"),
+        new Student(4,"Stud4","Stud4@gmail.com")
     };
 
     [HttpGet()]
