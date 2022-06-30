067d4a43a3bae2af0134b7318d7c0badcb9d9ebb
PC <201910310@ua.edu.lb>
2022-06-30 01:01:27 -0700

    Add missing files in master

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.OpenApi.Models;
 using WebApplication2.Abstraction;
+using WebApplication2.Filter;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>  
+{  
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sample.FileUpload.Api", Version = "v1" });  
+    c.OperationFilter<SwaggerFileOperationFilter>();  
+});
 builder.Services.AddTransient<IStudentHelper, StudentHelper1>();
 
 var app = builder.Build();
@@ -20,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
